a1b3e45bad430d13cc78245d0a309b5913bc76d6
Appala.Hima Varshini Sai <himavarshinisai.a2004@gmail.com>
2024-07-18 14:37:40 +0530

    Create 7)Write a program to print  the LCM of two numbers.c

---
+++ 7)Write a program to print  the LCM of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int x , y , max;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    max = (x > y) ? x : y;
+    while (1) 
+    {
+        if (max % x == 0 && max % y == 0)
+        {
+            printf("The LCM of %d and %d is %d.", x, y,max);
+            break;
+        }
+        ++max;
+    }
+}
